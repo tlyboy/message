5a9b37c47a9ae2d2479a8cfa82ed38cc77d53fc7
Guany <1084072690@qq.com>
2022-07-24 21:08:38 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# message
+
+基于 Chalk 的 Message 提示。
+
+## 使用说明
+
+```javascript
+import message from '@tlyboy/message'
+```
+
+## 使用许可
+
+[MIT](https://opensource.org/licenses/MIT) © Guany
---
+++ main.js
@@ -0,0 +1,17 @@
+import chalk from 'chalk'
+
+export default {
+  config(options) {},
+  info(content, title = 'Info', color = chalk.black.bgBlue) {
+    console.log(`${color(` ${title} `)}${!content ? '' : ` ${content}`}`)
+  },
+  success(content, title = 'Success', color = chalk.black.bgGreen) {
+    this.info(content, title, color)
+  },
+  error(content, title = 'Error', color = chalk.black.bgRed) {
+    this.info(content, title, color)
+  },
+  warning(content, title = 'Warning', color = chalk.black.bgYellow) {
+    this.info(content, title, color)
+  }
+}
