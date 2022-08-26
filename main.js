512cee1f1421b2f4b5c49642763b55c754f232e4
Guany <1084072690@qq.com>
2022-08-26 19:52:39 +0800

    feat: add message

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-// ESM syntax is supported.
-export {}
+import chalk from 'chalk'
+
+export default {
+  info(message) {
+    console.log(`${chalk.black.bgBlue(' INFO ')} ${message}`)
+  },
+  success(message) {
+    console.log(`${chalk.black.bgGreen(' DONE ')} ${message}`)
+  },
+  error(message) {
+    console.log(`${chalk.black.bgRed(' ERROR ')} ${message}`)
+  },
+  warning(message) {
+    console.log(`${chalk.black.bgYellow(' WARN ')} ${message}`)
+  }
+}
