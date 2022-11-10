a41a94c5eae130cc4e269406448a2db2b2ccc210
Guany <1084072690@qq.com>
2022-11-10 11:57:03 +0800

    feat: add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+declare const message: {
+  info(message: string): void
+  success(message: string): void
+  error(message: string): void
+  warning(message: string): void
+}
+
+export default message
